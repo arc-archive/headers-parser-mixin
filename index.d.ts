4d81e725df69e5278fa716a1b6b2f1664f7c4f85
Pawel <jarrodek@gmail.com>
2020-04-14 22:46:15 -0700

    chore: updating types

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@
  *   https://github.com/Polymer/tools/tree/master/packages/gen-typescript-declarations
  *
  * To modify these typings, edit the source file(s):
- *   headers-parser-mixin.js
+ *   index.js
  */
 
 
